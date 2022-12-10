87028667b444df23bd0e2504e8eb0ec557424e60
Kanishka Ganguly <kanishkaganguly2002@gmail.com>
2022-12-10 00:22:00 -0500

    Push part 8

--- Part8/2022_8.py
+++ Part8/2022_8.py
@@ -1,18 +1,14 @@
 #/usr/bin/env python3
-input_txt = "2022_8.txt"
+input_txt = "Part8/2022_8.txt"
 
-
-'''
-30373
-25512
-65332
-33549
-35390
-'''
 import numpy as np
 
-forest = np.array([[3, 0, 3, 7, 3], [2, 5, 5, 1, 2], [6, 5, 3, 3, 2],
+forest_test = np.array([[3, 0, 3, 7, 3], [2, 5, 5, 1, 2], [6, 5, 3, 3, 2],
                    [3, 3, 5, 4, 9], [3, 5, 3, 9, 0]])
+
+with open(input_txt) as f:
+	forest = np.array([list(map(int, line.strip())) for line in f])
+print(forest)
 
 w, h = np.shape(forest)
 
@@ -29,16 +25,57 @@ for i in range(1, w_inner):
 	print()
 print("------------------")
 
-top_arr = [forest[(3 - x)][3] for x in range(1, 4)]
-print(top_arr)
-bottom_arr = [forest[(3 + x)][3] for x in range(3, w)]
+interior = 0
+exterior = 0
+exterior = w*2 + (h-2)*2
+max_scenic_score = 0
 
-# for i in range(1, w_inner):
-# 	for j in range(1, h_inner):
-# 		top_arr = [forest[i - x][j] for x in range(0, i)]
-# 		bottom_arr = [forest[i + x][j] for x in range(i, w)]
-# 		print(f"{forest[i][j]}: {top_arr} {bottom_arr}")
-# if any([x < forest[i][j] for x in dirs]):
-# 	print(f"{forest[i][j]}: {top}{bottom}{left}{right} [VISIBLE]")
-# else:
-# 	print(f"{forest[i][j]}: {top}{bottom}{left}{right} [NOT VISIBLE]")
+for i in range(1, w_inner):
+	for j in range(1, h_inner):
+		column = forest[:, j]
+		row = forest[i, :]	
+		top = list(reversed(column[:i]))
+		bottom = column[i+1:]
+		left = list(reversed(row[:j]))
+		right = row[j+1:]
+		visible = any(
+			[all(x < forest[i][j] for x in top),
+			all(x < forest[i][j] for x in bottom),
+			all(x < forest[i][j] for x in left),
+			all(x < forest[i][j] for x in right)]
+		)
+		if visible:
+			interior += 1
+
+		score_top, score_bottom, score_left, score_right = 0, 0, 0, 0
+		for x in top:
+			if x < forest[i][j]:
+				score_top += 1
+			else:
+				score_top += 1
+				break
+		for x in bottom:
+			if x < forest[i][j]:
+				score_bottom += 1
+			else:
+				score_bottom += 1
+				break
+		for x in left:
+			if x < forest[i][j]:
+				score_left += 1
+			else:
+				score_left += 1
+				break
+		for x in right:
+			if x < forest[i][j]:
+				score_right += 1
+			else:
+				score_right += 1
+				break
+
+		scenic_score = score_top * score_bottom * score_left * score_right 
+		max_scenic_score = scenic_score if scenic_score > max_scenic_score else max_scenic_score
+
+
+print(f"Part1: {interior + exterior}")
+print(f"Part2: {max_scenic_score}")
